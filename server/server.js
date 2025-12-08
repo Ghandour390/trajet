69116a9eb09afbec2c0f27fe872232f36504b8b6
ghandour <abdelhakghandour6@gmail.com>
2025-12-08 16:21:40 +0100

    modifie 4 CI.yml

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandler.js';
 import userRoutes from './routes/userRoutes.js';
-import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
 
 // Error Handler
 app.use(errorHandler);
